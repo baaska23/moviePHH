963ce2adbadc0858bd39cde3b625404a8fc91544
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2024-11-23 18:42:53 +0800

    second Baaska

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Wefureguyee</h1>
+      <h2>Baaska bn</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
